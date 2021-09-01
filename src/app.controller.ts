9a2652b6ebbe5dc47d0bc168dfee96ee29b8dd59
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-09-01 22:33:49 +0900

    Resolved ddependencies problem.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const responseData = {
+      message: this.appService.getHello(),
+    };
+
+    return responseData;
   }
 }
